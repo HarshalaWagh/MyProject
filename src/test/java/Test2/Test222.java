2bb90a0f5c0ac08f712809103c02021adbe0f77d
HarshalaWagh <erharshalawagh@gmail.com>
2022-07-11 20:21:16 +0200

    myTest 2 branch

---
+++ src/test/java/Test2/Test222.java
@@ -0,0 +1,15 @@
+package Test2;
+
+public class Test222 {
+
+
+		public static void main(String[] args) {
+			int num1=123,num2=321,Mul=0;
+			for(int i=1;i<=num2;i++) {
+				Mul=Mul+num1;
+			}
+			System.out.println(Mul);
+		}
+	}
+
+
